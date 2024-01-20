f83f2eb07a0f95198223f0bd412682614fce2891
KuMMii <128502524+KuMMii@users.noreply.github.com>
2024-01-21 00:34:00 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -1,43 +1,25 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int N, len;
-        char[][] cmd;
+        int N;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        String tmp = br.readLine();
-        len = tmp.length();
-        cmd = new char[N][len];
-        cmd[0] = tmp.toCharArray();
-        char[] ans = tmp.toCharArray();
+        char[] ans = br.readLine().toCharArray();
 
         if (N > 1) {
             for (int i = 1; i < N; i++) {
-                cmd[i] = br.readLine().toCharArray();
-            }
-
-
-            //알파벳 iterate
-            for (int i = 0; i < len; i++) {
-                //글자 iterate
-                for (int j = 1; j < N; j++) {
-                    if (cmd[0][i] == cmd[j][i]) ans[i] = cmd[0][i];
-                    else {
-                        ans[i] = '?';
-                        break;
-                    }
+                char[] compare = br.readLine().toCharArray();
+                for (int j = 0; j < ans.length; j++) {
+                    if (ans[j] != compare[j]) ans[j] = '?';
                 }
             }
         }
 
-        for (char an : ans) {
-            System.out.print(an);
-        }
+        System.out.println(ans);
 
     }
 }
